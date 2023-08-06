2cfaa049109854965fe5617960e6fce5c8246d17
jsj3282 <jeongseonju15@gmail.com>
2023-08-06 22:33:07 +0900

    commit

---
+++ src/main/java/org/example/programmers/recursion/쿼드압축_후_개수_세기.java
@@ -0,0 +1,53 @@
+package org.example.programmers.recursion;
+
+import java.util.Arrays;
+
+class 쿼드압축_후_개수_세기 {
+
+    // 하나의 재귀에서는 해당 상태가 나타내는 정사각형을 압축했을 때 남아 있는 0과 1의 개수를 구해야 한다.
+    private static class Count {
+        public final int zero;
+        public final int one;
+
+        public Count(int zero, int one) {
+            this.zero = zero;
+            this.one = one;
+        }
+
+        public Count add(Count other) {
+            return new Count(zero + other.zero, one + other.one);
+        }
+    }
+
+    private Count count(int offsetX, int offsetY, int size, int[][] arr) {
+        int h = size / 2;
+        for (int x = offsetX; x < offsetX + size; x++) {
+            for (int y = offsetY; y < offsetY + size; y++) {
+                if (arr[y][x] != arr[offsetY][offsetX]) {
+                    return count(offsetX, offsetY, h, arr)   // 원소가 섞여 있는 경우 반환값
+                            .add(count(offsetX + h, offsetY, h, arr))
+                            .add(count(offsetX, offsetY + h, h, arr))
+                            .add(count(offsetX + h, offsetY + h, h, arr));
+                }
+            }
+        }
+        // 모든 원소가 같은 값인 경우
+        if (arr[offsetY][offsetX] == 1) {
+            return new Count(0, 1);
+        }
+        return new Count(1, 0);
+    }
+
+    public int[] solution(int[][] arr) {
+        Count count = count(0, 0, arr.length, arr);
+        return new int[] {count.zero, count.one};
+    }
+}
+
+class Main116 {
+    public static void main(String[] args) {
+        쿼드압축_후_개수_세기 problem = new 쿼드압축_후_개수_세기();
+        int[][] arr = {{1, 1, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 1}, {1, 1, 1, 1}};
+        System.out.println(Arrays.toString(problem.solution(arr)));
+    }
+}
