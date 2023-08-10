f848a82d7ce4452a8070cab0446978b2a646d627
jsj3282 <jeongseonju15@gmail.com>
2023-08-11 01:23:47 +0900

    commit

--- src/main/java/org/example/programmers/string/문자열_다루기_기본.java
+++ src/main/java/org/example/programmers/string/문자열_다루기_기본.java
@@ -4,6 +4,17 @@ class 문자열_다루기_기본 {
     public boolean solution(String s) {
         return s.matches("[0-9]{4}|[0-9]{6}");
     }
+
+    public boolean solution2(String s) {
+        if ((s.length() == 4 || s.length() == 6)) {
+            try {
+                int x = Integer.parseInt(s);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        } else return false;
+    }
 }
 
 class Main114 {
