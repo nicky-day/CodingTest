4d779e87c824ff03500e8cf201fe9434afdfa7d7
jsj3282 <jeongseonju15@gmail.com>
2023-08-31 21:01:04 +0900

    commit

---
+++ src/main/java/org/example/programmers/hash/없는_숫자_더하기.java
@@ -0,0 +1,29 @@
+package org.example.programmers.hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class 없는_숫자_더하기 {
+
+    public int solution(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (int v : numbers) {
+            set.add(v);
+        }
+
+        int sum = 0;
+        for (int n = 0; n <= 9; n++) {
+            if (set.contains(n)) continue;
+            sum += n;
+        }
+        return sum;
+    }
+}
+
+class Main139 {
+    public static void main(String[] args) {
+        없는_숫자_더하기 problem = new 없는_숫자_더하기();
+        int[] numbers = {1, 2, 3, 4, 6, 7, 8, 0};
+        System.out.println(problem.solution(numbers));
+    }
+}
