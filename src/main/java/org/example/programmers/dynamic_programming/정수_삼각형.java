2b12739f1b972fd61dbe4cb91041f6bc702a455b
jsj3282 <jeongseonju15@gmail.com>
2023-09-13 22:25:12 +0900

    commit

---
+++ src/main/java/org/example/programmers/dynamic_programming/정수_삼각형.java
@@ -0,0 +1,31 @@
+package org.example.programmers.dynamic_programming;
+
+import java.util.Arrays;
+
+class 정수_삼각형 {
+    private final int[][] mem = new int[501][501];
+
+    private int max(int x, int y, int[][] triangle) {
+        if (y == triangle.length) return 0;
+        if (mem[x][y] != -1) return mem[x][y];
+
+        return mem[x][y] = triangle[y][x] + Math.max(
+                max(x, y + 1, triangle),
+                max(x + 1, y + 1, triangle));
+    }
+
+    public int solution(int[][] triangle) {
+        for (int[] row : mem) {
+            Arrays.fill(row, -1);
+        }
+        return max(0, 0, triangle);
+    }
+}
+
+class Main142 {
+    public static void main(String[] args) {
+        정수_삼각형 problem = new 정수_삼각형();
+        int[][] triangle = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
+        System.out.println(problem.solution(triangle));
+    }
+}
