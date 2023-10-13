c2ba6f004e5e5e51fefb991fe9e8a9c366ae97ea
jsj3282 <jeongseonju15@gmail.com>
2023-10-13 23:01:58 +0900

    commit

--- README.md
+++ README.md
@@ -170,6 +170,7 @@
 ----------
 - 우선순위 큐
   - 이중 우선순위 큐(https://school.programmers.co.kr/learn/courses/30/lessons/42628)
+  - 디스크 컨트롤러(https://school.programmers.co.kr/learn/courses/30/lessons/42627)
 ----------
 - 순열과 조합
   - [구슬을 나누는 경우의 수](https://school.programmers.co.kr/learn/courses/30/lessons/120840)
---
+++ src/main/java/org/example/programmers/priority_queue/디스크_컨트롤러.java
@@ -0,0 +1,55 @@
+package org.example.programmers.priority_queue;
+
+import java.util.*;
+
+class 디스크_컨트롤러 {
+    private static class Job {
+        public final int start;
+        public final int duration;
+
+        private Job(int start, int duration) {
+            this.start = start;
+            this.duration = duration;
+        }
+    }
+
+    public int solution(int[][] rawJobs) {
+        Job[] jobs = new Job[rawJobs.length];
+        for (int i = 0; i < rawJobs.length; i++) {
+            jobs[i] = new Job(rawJobs[i][0], rawJobs[i][1]);
+        }
+        Arrays.sort(jobs, Comparator.comparingInt(job -> job.start));
+
+        Queue<Job> q = new LinkedList<>(Arrays.asList(jobs));
+        PriorityQueue<Job> pq = new PriorityQueue<>(Comparator.comparingInt(job -> job.duration));
+
+        int exec = 0;       // 작업의 요청 시간부터 종료 시간까지 누적합
+        int time = 0;       // 현재 시간
+
+        while (!q.isEmpty() || !pq.isEmpty()) {
+            // 작업 처리
+            while (!q.isEmpty() && q.peek().start <= time) {
+                pq.add(q.poll());
+            }
+
+            if (pq.isEmpty()) {
+                time = q.peek().start;
+                continue;
+            }
+
+            Job job = pq.poll();
+            exec += time + job.duration - job.start;
+            time += job.duration;
+        }
+
+        return exec / jobs.length;
+    }
+}
+
+class Main154 {
+    public static void main(String[] args) {
+        디스크_컨트롤러 problem = new 디스크_컨트롤러();
+        int[][] jobs = {{0, 3}, {1, 9}, {2, 6}};
+        System.out.println(problem.solution(jobs));
+    }
+}
