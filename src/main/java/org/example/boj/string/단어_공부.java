ec3a590f9d4ea00ff46d576b883aab764efac602
jsj3282 <jeongseonju15@gmail.com>
2023-11-19 20:08:35 +0900

    commit

--- src/main/java/org/example/boj/string/단어_공부.java
+++ src/main/java/org/example/boj/string/단어_공부.java
@@ -1,10 +1,5 @@
 package org.example.boj.string;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
 class 단어_공부 {
     public char solution(String word) {
         word = word.toLowerCase();
@@ -25,39 +20,37 @@ class 단어_공부 {
         }
         return (char) (maxIndex + 65);
     }
+
+    public void solution2(String str) {
+        str = str.toUpperCase();
+        int[] count = getAlphabetCount(str);
+
+        int maxCount = -1;
+        char maxAlphabet = '?';
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > maxCount) {
+                maxCount = count[i];
+                maxAlphabet = (char) ('A' + i);
+            } else if (count[i] == maxCount) {
+                maxAlphabet = '?';
+            }
+        }
+        System.out.println(maxAlphabet);
+    }
+
+    public int[] getAlphabetCount(String str) {
+        int[] count = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i) - 'A']++;
+        }
+        return count;
+    }
 }
 
 class Main160 {
-    public static void main(String[] args) throws IOException {
-//        단어_공부 problem = new 단어_공부();
-//        String word = "Mississipi";
-//        String word2 = "zZa";
-//        System.out.println(problem.solution(word2));
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String word = br.readLine();
-        word = word.toLowerCase();
-        int[] array = new int[26];
-        for (char c : word.toCharArray()) {
-            array[c - 97] += 1;
-        }
-
-        int max = Integer.MIN_VALUE;
-        int maxIndex = -1;
-        boolean duplicate = false;
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
-                maxIndex = i;
-            }
-        }
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == max && maxIndex != i) {
-                duplicate = true;
-            }
-        }
-        System.out.println(duplicate ? '?' : (char)(maxIndex + 65));
+    public static void main(String[] args) {
+        단어_공부 problem = new 단어_공부();
+        String word = "Mississipi";
+        problem.solution2(word);
     }
 }
