10bb0a1ddcf47e791b9ad76c55ac7a8dc506a5e2
jsj3282 <jeongseonju15@gmail.com>
2024-03-10 23:19:02 +0900

    commit

---
+++ src/main/java/org/example/boj/simulation/ACM_호텔.java
@@ -0,0 +1,25 @@
+package org.example.boj.simulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class ACM_호텔 {
+}
+
+class Main216 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            String[] input = br.readLine().split(" ");
+            int H = Integer.parseInt(input[0]);
+            int W = Integer.parseInt(input[1]);
+            int N = Integer.parseInt(input[2]);
+
+            int floor = ((N - 1) % H) + 1;
+            int number = (N - 1) / H + 1;
+            System.out.printf("%d%02d\n", floor, number);
+        }
+    }
+}
