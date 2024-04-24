92ffe6f6026716f73715ea520a7591b3aa1788a4
jsj3282 <jeongseonju15@gmail.com>
2024-04-24 12:34:02 +0900

    commit

--- src/main/java/org/example/boj/prefix_sum/구간_합_구하기_5.java
+++ src/main/java/org/example/boj/prefix_sum/구간_합_구하기_5.java
@@ -41,3 +41,35 @@ class Main232 {
         bw.flush();
     }
 }
+
+class Main233 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int[][] acc = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                acc[i][j] = acc[i][j - 1] + acc[i - 1][j] - acc[i - 1][j - 1] + arr[i][j];
+            }
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        while(m-- > 0) {
+            int ans = 0;
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+            ans = acc[x2][y2] - acc[x1 - 1][y2] - acc[x2][y1 - 1] + acc[x1 - 1][y1 - 1];
+            bw.write(ans + "\n");
+        }
+        bw.flush();
+    }
+}
