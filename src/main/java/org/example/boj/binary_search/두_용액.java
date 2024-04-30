fad2f6a1e3a86da818800cd571fbad3ec2931da8
jsj3282 <jeongseonju15@gmail.com>
2024-05-01 00:10:14 +0900

    commit

--- src/main/java/org/example/boj/binary_search/두_용액.java
+++ src/main/java/org/example/boj/binary_search/두_용액.java
@@ -1,9 +1,10 @@
 package org.example.boj.binary_search;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 class 두_용액 {
 }
@@ -50,5 +51,36 @@ class Main244 {
             }
         }
         System.out.println(ans1 + " " + ans2);
+    }
+}
+
+class Main245 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int ansAbs = 2000000001;
+        int ans1 = 0;
+        int ans2 = 0;
+        TreeSet<Integer> set = new TreeSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            Integer[] pairValues = {set.floor(-x), set.ceiling(-x)};
+            for (Integer pairValue : pairValues) {
+                if (pairValue == null) continue;
+                int sumAbs = Math.abs(x + pairValue);
+                if (ansAbs > sumAbs) {
+                    ansAbs = sumAbs;
+                    ans1 = x;
+                    ans2 = pairValue;
+                }
+            }
+            set.add(x);
+        }
+        bw.write(Math.min(ans1, ans2) + " " + Math.max(ans1, ans2));
+        bw.flush();
     }
 }
