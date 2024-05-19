69840916895cf23e93fa193a9eea3e4e2f761b04
jsj3282 <jeongseonju15@gmail.com>
2024-05-19 22:14:01 +0900

    commit

--- src/main/java/org/example/recursion_permutaion_and_combination/boj/003-N과_M(7).java
+++ src/main/java/org/example/recursion_permutaion_and_combination/boj/003-N과_M(7).java
@@ -3,24 +3,23 @@ package org.example.recursion_permutaion_and_combination.boj;
 import java.util.Arrays;
 import java.util.Scanner;
 
-class N과_M_6 {
+class N과_M_7 {
 }
 
-class Main2 {
+class Main3 {
 
     public static int[] numbers;
     public static int[] output;
-    public static boolean[] check;
+    public static StringBuilder sb = new StringBuilder();
 
     public static void print(int[] arr, int n) {
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             sb.append(arr[i]).append(" ");
         }
-        System.out.println(sb);
+        sb.append("\n");
     }
 
-    public static void permutation(int depth, int n, int m, int start) {
+    public static void permutation(int depth, int n, int m) {
         // base case
         if (depth == m) {
             print(output, m);
@@ -28,13 +27,9 @@ class Main2 {
         }
 
         // recursive case
-        for (int i = start; i < n; i++) {
-            if (!check[i]) {
-                check[i] = true;
-                output[depth] = numbers[i];
-                permutation(depth + 1, n, m, i + 1);
-                check[i] = false;
-            }
+        for (int i = 0; i < n; i++) {
+            output[depth] = numbers[i];
+            permutation(depth + 1, n, m);
         }
     }
 
@@ -45,12 +40,12 @@ class Main2 {
 
         numbers = new int[n];
         output = new int[m];
-        check = new boolean[n];
         for (int i = 0; i < n; i++) {
             numbers[i] = sc.nextInt();
         }
         Arrays.sort(numbers);
 
-        permutation(0, n, m, 0);
+        permutation(0, n, m);
+        System.out.println(sb);
     }
 }
