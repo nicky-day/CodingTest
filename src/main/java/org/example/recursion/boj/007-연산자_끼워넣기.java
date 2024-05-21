f1585d47b3483b6c7de3218e5456f677c2ee5715
jsj3282 <jeongseonju15@gmail.com>
2024-05-21 23:06:42 +0900

    commit

---
+++ src/main/java/org/example/recursion/boj/007-연산자_끼워넣기.java
@@ -0,0 +1,68 @@
+package org.example.recursion.boj;
+
+import java.util.Scanner;
+
+class 연산자_끼워넣기 {
+}
+
+class Main7 {
+
+    public static final int PLUS = 0;
+    public static final int MINUS = 1;
+    public static final int MUL = 2;
+    public static final int DIV = 3;
+
+    public static int n;
+    public static int[] numbers;
+    public static int[] operators = new int[4]; // +, -, *, /
+
+    public static int max = Integer.MIN_VALUE;
+    public static int min = Integer.MAX_VALUE;
+
+    public static void solve(int index, int sum) {
+        // base case
+        if (index == n) {
+            if (sum > max) max = sum;
+            if (sum < min) min = sum;
+            return;
+        }
+
+        // recursive case
+        for (int i = 0; i < 4; i++) {
+            if (operators[i] > 0) {
+                operators[i]--;
+                switch (i) {
+                    case PLUS:
+                        solve(index + 1, sum + numbers[index]);
+                        break;
+                    case MINUS:
+                        solve(index + 1, sum - numbers[index]);
+                        break;
+                    case MUL:
+                        solve(index + 1, sum * numbers[index]);
+                        break;
+                    case DIV:
+                        solve(index + 1, sum / numbers[index]);
+                        break;
+                }
+                operators[i]++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 4; i++) {
+            operators[i] = sc.nextInt();
+        }
+
+        solve(1, numbers[0]);
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
